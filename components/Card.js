1237abb6b0e19953d15f455b2bba9a4650d904f0
christian89-alt <c.duboc43@gmail.com>
2022-07-23 21:32:29 +0200

    Modification css pour le composant card

--- components/Card.js
+++ components/Card.js
@@ -1,32 +1,79 @@
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
 
+const Card = () => {
+  return (
+    <div class="card">
+      <div class="card-image" image is-4by3>
+        <Image src="/cardjavascript.png" width={300} height={300} />
+        <h2>Conseil en stratégie digitale</h2>
+        <p>
+          Dès sa création, nous concevons votre site internet pour qu'il évolue
+          avec votre entreprise et qu'il vous accompagne dans le développement
+          de votre chiffre d’affaires. En développant les fonctionnalités de
+          votre site internet celui-ci peut également être connecté à de
+          nombreuses interfaces comme des outils statistiques, vos réseaux
+          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
+          la création de votre site et développez votre entreprise!!
+        </p>
+      </div>
+      <div class="card-image" image is-4by3>
+        <Image src="/cardjavascript.png" width={300} height={300} />
+        <h2>Conseil en stratégie digitale</h2>
+        <p>
+          Dès sa création, nous concevons votre site internet pour qu'il évolue
+          avec votre entreprise et qu'il vous accompagne dans le développement
+          de votre chiffre d’affaires. En développant les fonctionnalités de
+          votre site internet celui-ci peut également être connecté à de
+          nombreuses interfaces comme des outils statistiques, vos réseaux
+          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
+          la création de votre site et développez votre entreprise!!
+        </p>
+      </div>
 
-const Card  = () => {
-    return ( 
+      <div class="card-image" image is-4by3>
+        <Image src="/cardjavascript.png" width={300} height={300} />
+        <h2>Conseil en stratégie digitale</h2>
+        <p>
+          Dès sa création, nous concevons votre site internet pour qu'il évolue
+          avec votre entreprise et qu'il vous accompagne dans le développement
+          de votre chiffre d’affaires. En développant les fonctionnalités de
+          votre site internet celui-ci peut également être connecté à de
+          nombreuses interfaces comme des outils statistiques, vos réseaux
+          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
+          la création de votre site et développez votre entreprise!!
+        </p>
+      </div>
+      <div class="card-image" image is-4by3>
+        <Image src="/cardjavascript.png" width={300} height={300} />
+        <h2>Conseil en stratégie digitale</h2>
+        <p>
+          Dès sa création, nous concevons votre site internet pour qu'il évolue
+          avec votre entreprise et qu'il vous accompagne dans le développement
+          de votre chiffre d’affaires. En développant les fonctionnalités de
+          votre site internet celui-ci peut également être connecté à de
+          nombreuses interfaces comme des outils statistiques, vos réseaux
+          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
+          la création de votre site et développez votre entreprise!!
+        </p>
+      </div>
+      <div class="card-image" image is-4by3>
+        <Image src="/cardjavascript.png" width={300} height={300} />
+        <h2>Conseil en stratégie digitale</h2>
+        <p>
+          Dès sa création, nous concevons votre site internet pour qu'il évolue
+          avec votre entreprise et qu'il vous accompagne dans le développement
+          de votre chiffre d’affaires. En développant les fonctionnalités de
+          votre site internet celui-ci peut également être connecté à de
+          nombreuses interfaces comme des outils statistiques, vos réseaux
+          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
+          la création de votre site et développez votre entreprise!!
+        </p>
+      </div>
+    </div>
 
-        <div class="card">
-  <div class="card-image" image is-4by3>
     
-  <Image src="/cardjavascript.png" width={300} height={300} />
-    <p>Dès sa création, nous concevons votre site internet pour qu'il évolue avec votre entreprise et qu'il vous accompagne dans le développement de votre chiffre d’affaires. En développant les fonctionnalités de votre site internet celui-ci peut également être connecté à de nombreuses interfaces comme des outils statistiques, vos réseaux sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous la création de votre site et développez votre entreprise!!</p>
-  </div>
-  <div class="card-image" image is-4by3>
-    
-  <Image src="/cardjavascript.png" width={300} height={300} />
-    <p>Dès sa création, nous concevons votre site internet pour qu'il évolue avec votre entreprise et qu'il vous accompagne dans le développement de votre chiffre d’affaires. En développant les fonctionnalités de votre site internet celui-ci peut également être connecté à de nombreuses interfaces comme des outils statistiques, vos réseaux sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous la création de votre site et développez votre entreprise!!</p>
-  </div>
+  );
+};
 
-  <div class="card-image" image is-4by3>
-    
-  <Image src="/cardjavascript.png" width={300} height={300} />
-    <p>Dès sa création, nous concevons votre site internet pour qu'il évolue avec votre entreprise et qu'il vous accompagne dans le développement de votre chiffre d’affaires. En développant les fonctionnalités de votre site internet celui-ci peut également être connecté à de nombreuses interfaces comme des outils statistiques, vos réseaux sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous la création de votre site et développez votre entreprise!!</p>
-  </div>
-  
-</div>
-
-
-     );
-}
- 
-export default Card ;
+export default Card;
