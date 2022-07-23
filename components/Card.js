442140acc0b401aee78e3f976e9b2d6aca0e502a
christian89-alt <c.duboc43@gmail.com>
2022-07-23 22:25:01 +0200

    Mise en place composant slider avec react responsive carousel

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import style from "../styles/Home.module.css"
 
 const Card = () => {
   return (
+   
     <div class="card">
       <div class="card-image" image is-4by3>
         <Image src="/cardjavascript.png" width={300} height={300} />
-        <h2>Conseil en stratégie digitale</h2>
+        <h2>Création de site </h2>
         <p>
           Dès sa création, nous concevons votre site internet pour qu'il évolue
           avec votre entreprise et qu'il vous accompagne dans le développement
@@ -19,7 +21,7 @@ const Card = () => {
       </div>
       <div class="card-image" image is-4by3>
         <Image src="/cardjavascript.png" width={300} height={300} />
-        <h2>Conseil en stratégie digitale</h2>
+        <h2>Stratégie digitale</h2>
         <p>
           Dès sa création, nous concevons votre site internet pour qu'il évolue
           avec votre entreprise et qu'il vous accompagne dans le développement
@@ -33,7 +35,7 @@ const Card = () => {
 
       <div class="card-image" image is-4by3>
         <Image src="/cardjavascript.png" width={300} height={300} />
-        <h2>Conseil en stratégie digitale</h2>
+        <h2>Création d'entreprise</h2>
         <p>
           Dès sa création, nous concevons votre site internet pour qu'il évolue
           avec votre entreprise et qu'il vous accompagne dans le développement
@@ -44,33 +46,10 @@ const Card = () => {
           la création de votre site et développez votre entreprise!!
         </p>
       </div>
-      <div class="card-image" image is-4by3>
-        <Image src="/cardjavascript.png" width={300} height={300} />
-        <h2>Conseil en stratégie digitale</h2>
-        <p>
-          Dès sa création, nous concevons votre site internet pour qu'il évolue
-          avec votre entreprise et qu'il vous accompagne dans le développement
-          de votre chiffre d’affaires. En développant les fonctionnalités de
-          votre site internet celui-ci peut également être connecté à de
-          nombreuses interfaces comme des outils statistiques, vos réseaux
-          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
-          la création de votre site et développez votre entreprise!!
-        </p>
-      </div>
-      <div class="card-image" image is-4by3>
-        <Image src="/cardjavascript.png" width={300} height={300} />
-        <h2>Conseil en stratégie digitale</h2>
-        <p>
-          Dès sa création, nous concevons votre site internet pour qu'il évolue
-          avec votre entreprise et qu'il vous accompagne dans le développement
-          de votre chiffre d’affaires. En développant les fonctionnalités de
-          votre site internet celui-ci peut également être connecté à de
-          nombreuses interfaces comme des outils statistiques, vos réseaux
-          sociaux, un CRM, des solutions de réservations en ligne. Confiez-nous
-          la création de votre site et développez votre entreprise!!
-        </p>
-      </div>
+   
     </div>
+  
+
 
     
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image"
 
 
+
 const Navbar =()=> {
 
   return(
